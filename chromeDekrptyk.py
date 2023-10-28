053008635571ea3fa3962746ff9854d083ba4007
Krptyk <149233352+Krptyk@users.noreply.github.com>
2023-10-28 18:34:15 +1000

    Update chromeDekrptyk.py

--- chromeDekrptyk.py
+++ chromeDekrptyk.py
@@ -19,43 +19,92 @@ def decrypt_value(buff, master_key):
     except:
         return "Chrome < 80"
 
+def decrypt_password(buff, master_key):
+    try:
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = AES.new(master_key, AES.MODE_GCM, iv)
+        decrypted_pass = cipher.decrypt(payload)[:-16].decode()
+        return decrypted_pass
+    except:
+        return "Chrome < 80"
+
+def display_credentials(url, username, decrypted_password):
+    separator = "-" * 60
+    print(separator)
+    print(f"URL: {url}")
+    print(f"User Name: {username}")
+    print(f"Password: {decrypted_password}")
+    print(separator)
+    print("\n")
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Retrieve Encrypted Chrome Cookie Details.")
-    parser.add_argument("-f", "--file", required=True, help="Path to the Chrome Cookies database.")
-    parser.add_argument("-k", "--key", required=True, help="Path to the master key file.")
+    parser = argparse.ArgumentParser(description="Retrieve Encrypted Chrome Details.")
+    
+    parser.add_argument(
+        "-f", "--file", 
+        required=True, 
+        help="""Path to the Chrome database. 
+For 'cookies', provide the path to the 'Cookies' SQLite database.
+For 'login', provide the path to the 'Login Data' SQLite database."""
+    )
+    
+    parser.add_argument(
+        "-k", "--key", 
+        required=True, 
+        help="""Path to the master encryption key file.
+This key is crucial for decrypting the database content. 
+Typically found within the Chrome's 'User Data' directory under a file named 'Local State', but you should extract the key from it."""
+    )
+    
+    parser.add_argument(
+        "-t", "--type", 
+        choices=['cookies', 'login'], 
+        required=True, 
+        help="""Type of data to retrieve.
+- 'cookies': To retrieve encrypted cookies data.
+- 'login': To retrieve saved login credentials (usernames & passwords)."""
+    )
+    
     args = parser.parse_args()
 
     with open(args.key, 'rb') as f:
         master_key = f.read()
 
-    temp_db = "CookiesTemp.db"
+    temp_db = f"Temp_{args.type}.db"
     shutil.copy2(args.file, temp_db)
 
-    grouped_data = {}
-    
     with sqlite3.connect(temp_db) as conn:
         cursor = conn.cursor()
-        for row in cursor.execute("SELECT host_key, name, encrypted_value, creation_utc, last_access_utc, expires_utc FROM cookies"):
-            host_key = row[0]
-            data = {
-                'name': row[1],
-                'decrypted_value': decrypt_value(row[2], master_key),
-                'creation_utc': chrome_time_conversion(row[3]),
-                'last_access_utc': chrome_time_conversion(row[4]),
-                'expires_utc': chrome_time_conversion(row[5])
-            }
+
+        if args.type == "cookies":
+            grouped_data = {}
+            for row in cursor.execute("SELECT host_key, name, encrypted_value, creation_utc, last_access_utc, expires_utc FROM cookies"):
+                host_key = row[0]
+                data = {
+                    'name': row[1],
+                    'decrypted_value': decrypt_value(row[2], master_key),
+                    'creation_utc': chrome_time_conversion(row[3]),
+                    'last_access_utc': chrome_time_conversion(row[4]),
+                    'expires_utc': chrome_time_conversion(row[5])
+                }
+                grouped_data.setdefault(host_key, []).append(data)
             
-            if host_key not in grouped_data:
-                grouped_data[host_key] = []
-            grouped_data[host_key].append(data)
-    
-    for host, cookies in grouped_data.items():
-        print("=" * 70)
-        print(f"Host: {host}")
-        for cookie in cookies:
-            print("\n")
-            for key, val in cookie.items():
-                print(f"{key.title().replace('_', ' ')}: {val}")
-        print("=" * 70, "\n")
+            for host, cookies in grouped_data.items():
+                print("=" * 70)
+                print(f"Host: {host}")
+                for cookie in cookies:
+                    print("\n".join(f"{key.title().replace('_', ' ')}: {val}" for key, val in cookie.items()), "\n")
+                print("=" * 70, "\n")
+
+        elif args.type == "login":
+            for row in cursor.execute("SELECT action_url, origin_url, username_value, password_value FROM logins"):
+                action_url = row[0]
+                origin_url = row[1]
+                url = action_url if action_url else origin_url
+                username = row[2]
+                encrypted_password = row[3]
+                decrypted_password = decrypt_password(encrypted_password, master_key)
+                display_credentials(url, username, decrypted_password)
 
     os.remove(temp_db)
